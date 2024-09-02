6e9d7df10835720e3b49e38c356e9e55484f27a7
Roger Killer <roger.killer@advery.ch>
2024-09-02 11:44:04 +0200

    ✨ (app.vue): add navigation links in the app component for Home, Signup, and Login pages ♻️ (index.vue): remove redundant navigation links from the index page and clean up the code by removing unnecessary comments

--- app.vue
+++ app.vue
@@ -1,5 +1,11 @@
 <template>
   <div>
+    <!-- sample nav  -->
+    <nav>
+      <NuxtLink to="/">Home</NuxtLink>
+      <NuxtLink to="/signup">Signup</NuxtLink>
+      <NuxtLink to="/login">Login</NuxtLink>
+    </nav>
     <NuxtPage />
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,5 @@
 <template>
   <div>
-    <!-- sample nav  -->
-    <nav>
-      <router-link to="/">Home</router-link>
-      <router-link to="/signup">Signup</router-link>
-      <router-link to="/login">Login</router-link>
-    </nav>
     <h1>Home</h1>
     <ul>
       <li v-for="user in users" :key="user.id">
@@ -16,6 +10,7 @@
 </template>
 
 <script setup>
+
 const { data: users } = useFetch('/api/getUsers')
 // const users = ref([])
 
